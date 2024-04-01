7781933f2a8fe9ca6c95e2556df57f5ba25688b2
HeewonJo <heewonjo.me@gmail.com>
2024-04-02 00:57:51 +0900

    session03_2

---
+++ Phone/Phone_Prj/contacts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Phone
+
+def list(request):
+    Phones = Phone.objects.all()
+    return render(request, "Phone/list.html", {'Phones' : Phones})
+
+def result(request):
+    word = request.GET['word']
+    words = Phone.objects.filter(name__contains = word)
+    return render(request, "Phone/result.html", {'word' : word, 'contacts' : words})
