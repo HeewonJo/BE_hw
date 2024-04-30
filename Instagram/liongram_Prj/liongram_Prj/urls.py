f0ee349c0ce3ac789eed672cd39fc33e3987a41b
HeewonJo <heewonjo.me@gmail.com>
2024-05-01 02:12:50 +0900

    crud

--- Instagram/liongram_Prj/liongram_Prj/urls.py
+++ Instagram/liongram_Prj/liongram_Prj/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from posts import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.list, name="list"),
-    path('result/', views.result, name="result"),
+    path('', include('posts.urls')),
 ]
--- Instagram/liongram_Prj/posts/views.py
+++ Instagram/liongram_Prj/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.db.models import Q 
 
@@ -10,3 +10,33 @@ def result(request):
     word = request.GET['word']
     words = Post.objects.filter(Q(title__contains = word) | Q(content__contains = word)).order_by('-created_at')
     return render(request, "posts/result.html", {'word' : word, 'posts' : words})
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        post = Post.objects.create(
+            title = title,
+            content = content,
+        )
+        return redirect('list')
+    return render(request, 'posts/create.html')
+def detail(request, id):
+    post = get_object_or_404(Post, id = id)
+    post.increase_views()
+    return render(request, 'posts/detail.html', {'post' : post })
+
+def update(request, id):
+    post = get_object_or_404(Post, id = id)
+    if request.method == "POST":
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('detail', id)
+    return render(request, 'posts/update.html', {'post' : post})
+
+def delete(request, id):
+    post = get_object_or_404(Post, id = id)
+    post.delete()
+    return redirect('list')
